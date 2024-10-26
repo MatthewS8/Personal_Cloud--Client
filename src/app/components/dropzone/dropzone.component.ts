7b87ad71d59123fec9a172aa8a96bc826ef512e7
MatthewS8 <matthew-stefanelli@outlook.it>
2024-10-26 12:24:53 +0200

    feat(dropzone) added component to drag and drop files feat(card) added card to show uploading files in a prettier way

---
+++ src/app/components/dropzone/dropzone.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, ChangeDetectorRef, Component, ContentChild, ElementRef, EventEmitter, Output, Renderer2 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-dropzone',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dropzone.component.html',
+  styleUrl: './dropzone.component.scss'
+})
+export class DropzoneComponent implements AfterViewInit {
+  @ContentChild('content', { static: false }) content: ElementRef | undefined;
+  hasContent = false;
+  @Output() filesUploaded = new EventEmitter<FileList>();
+
+  constructor(private renderer: Renderer2, private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.hasContent = !!this.content;
+    this.cdr.detectChanges();
+  }
+  
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.renderer.addClass(event.target, 'dragover');
+  }
+
+  onDragLeave(event: DragEvent) {
+    this.renderer.removeClass(event.target, 'dragover');
+  }
+
+  onDrop(event: DragEvent) {
+    console.log('onDrop', event);
+    event.preventDefault();
+    this.renderer.removeClass(event.target, 'dragover');
+    const files = event.dataTransfer?.files;
+    if (files) {
+      this.filesUploaded.emit(files);
+    }
+  }
+
+}
---
+++ src/app/components/file-card/file-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, ContentChild, ElementRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-file-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './file-card.component.html',
+  styleUrl: './file-card.component.scss'
+})
+export class FileCardComponent {
+  @ContentChild('action', { static: false }) content: ElementRef | undefined;
+  @Input() title: string = '';
+}
